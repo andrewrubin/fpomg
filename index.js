e1ac683c315e4398fe64d022dbfb90ae506b6250
Andrew Rubin <andrew@wethecollective.com>
2021-11-13 15:11:22 -0800

    feat: change text color based on percieved brightness of background

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const createImage = require("./lib/create-image.js");
-const port = process.env.PORT || 8000;
+
+const isProd = process.env.NODE_ENV === "production" ? true : false;
+const port = isProd ? process.env.PORT : 8000;
 const app = express();
 
-app.get("/:width?/:height?/:color?", (req, res) => {
-  const image = createImage({ ...req.params, ...req.query });
-  res.writeHead(200, { "Content-Type": "image/png" });
-  res.end(image);
+app.get("/:width?/:height?/:color?", async (req, res) => {
+  try {
+    const image = await createImage({ ...req.params, ...req.query });
+    res.writeHead(200, { "Content-Type": "image/png" });
+    res.end(image);
+  } catch (err) {
+    if (!isProd) console.log(err);
+    res.send(err.message);
+  }
 });
 
 app.listen(port, () => console.log(`server running on port ${port}`));
