6d83bff570267320864db07bf9e68f4674b1a52a
Andrew Rubin <arubin78@gmail.com>
2021-11-13 12:31:58 -0800

    feat: add text input and custom color options

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const createImage = require("./lib/create-image.js");
-
+const port = process.env.PORT || 8000;
 const app = express();
 
-app.get("/:width?/:height?/:text?", (req, res) => {
-  res.send(createImage(req.params));
+app.get("/:width?/:height?/:color?", (req, res) => {
+  const image = createImage({ ...req.params, ...req.query });
+  res.writeHead(200, { "Content-Type": "image/png" });
+  res.end(image);
 });
 
-app.listen(3000, () => console.log("server running on port 3000"));
+app.listen(port, () => console.log(`server running on port ${port}`));
