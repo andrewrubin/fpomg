83db26d2bffcc1bc409a21bc41664258a272cb8f
Andrew Rubin <arubin78@gmail.com>
2021-11-14 22:28:57 -0800

    feat: start frontend build (WIP)

---
+++ frontend/index.js
@@ -0,0 +1,9 @@
+//- PayPal donate button scripts
+PayPal.Donation.Button({
+  env: "production",
+  hosted_button_id: "ECMDZR6C7KKYW",
+  image: {
+    src: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=",
+    alt: "",
+  },
+}).render("#donate-button");
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
 const createImage = require("./lib/create-image.js");
 
 const isProd = process.env.NODE_ENV === "production" ? true : false;
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 const app = express();
 
-app.get("/:width?/:height?/:color?", async (req, res) => {
+app.get("/:width/:height/:color?", async (req, res) => {
   try {
     const image = await createImage({ ...req.params, ...req.query });
     res.writeHead(200, { "Content-Type": "image/png" });
@@ -15,5 +16,9 @@ app.get("/:width?/:height?/:color?", async (req, res) => {
     res.send(err.message);
   }
 });
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/dist/index.html"))
+);
 
 app.listen(port, () => console.log(`server running on port ${port}`));
